058f388b1728578cfea8ae33db87d580f1906eb7
jltdev15 <75570239+jltdev15@users.noreply.github.com>
2024-02-20 22:29:28 +0800

    fix 2 router view links

--- src/ServicesContent/MobileApp.vue
+++ src/ServicesContent/MobileApp.vue
@@ -1,21 +1,21 @@
 <template>
-  <section class="py-20">
-    <div class="flex flex-row-reverse items-center justify-between p-6">
-      <div class="w-3/6">
+  <section class="sm:py-20">
+    <div class="flex flex-col items-center justify-between p-3 sm:p-6 sm:flex-row-reverse">
+      <div class="sm:w-3/6">
         <img
-          class="w-7/12 mx-auto"
+          class="w-3/6 mx-auto sm:w-4/6"
           src="../assets/images/app-dev-mobile.png"
           alt=""
         />
       </div>
-      <div class="w-3/6">
-        <h2 class="block text-6xl font-bold">
-          Mobile App<span class="block py-3 text-[#0245A3]"> Development</span>
+      <div class="text-center sm:w-3/6">
+        <h2 class="block font-bold sm:text-6xl">
+          Mobile App<span class="sm:block sm:py-3 text-[#0245A3]"> Development</span>
         </h2>
-        <p class="text-2xl font-semibold ">
+        <p class="text-xs font-semibold sm:text-2xl ">
           We have dedicated development Team
         </p>
-        <p class="py-3 text-lg">
+        <p class="py-3 text-xs text-justify sm:text-lg">
           Accelerate your business growth with cutting-edge mobile development
           solutions. Our expert team crafts high-performance, user-friendly
           mobile apps tailored to your unique business needs. From intuitive
@@ -25,21 +25,21 @@
           competitive market by harnessing the power of mobile technology to
           connect, engage, and thrive in the palm of your customers' hands.
         </p>
-        <div class="flex gap-5 pt-6">
+        <div class="flex flex-col-reverse gap-3 sm:pt-6 sm:gap-5 sm:flex-row">
           <button
-            class="flex capitalize items-center gap-3 py-3 px-6 outline outline-offset-1 outline-1  text-lg active:shadow-md transition-all font-bold text-[#0245A3] hover:text-[#0245a3b6]"
+            class="flex capitalize text-base justify-center items-center gap-3 py-3 px-6 outline outline-offset-1 outline-1  sm:text-lg active:shadow-md transition-all font-bold text-[#0245A3] hover:text-[#0245a3b6]"
           >
             Free consultation
             <i
-              class="text-2xl transition-all duration-150 fa-solid fa-envelope"
+              class="transition-all duration-150 sm:text-2xl fa-solid fa-envelope"
             ></i>
           </button>
           <button
-            class="flex capitalize items-center bg-[#0245A3] hover:bg-[rgba(2,69,163,0.8)] gap-3 py-3 px-6 text-lg  active:shadow-md transition-all font-bold text-gray-50 "
+            class="flex text-base border justify-center border-blue-800 capitalize items-center bg-[#0245A3] hover:bg-[rgba(2,69,163,0.8)] gap-3 py-3 px-6 sm:text-lg  active:shadow-md transition-all font-bold text-gray-50 "
           >
-            Check our rates
+            our rates
             <i
-              class="text-2xl transition-all duration-150 fa-solid fa-dollar-sign"
+              class="text-base transition-all duration-150 sm:text-2xl fa-solid fa-dollar-sign"
             ></i>
           </button>
         </div>
--- src/ServicesContent/SoftwareView.vue
+++ src/ServicesContent/SoftwareView.vue
@@ -1,14 +1,14 @@
 <template>
   <section class="sm:py-20">
-    <div class="flex flex-col-reverse items-center justify-between p-6 sm:flex-row">
-      <div class="sm:w-3/6">
+    <div class="flex flex-col-reverse items-center justify-between p-3 sm:p-6 sm:flex-row">
+      <div class="text-center sm:w-3/6">
         <h2 class="block font-bold sm:py-3 sm:text-6xl">
           Software<span class="sm:block sm:py-3 text-[#0245A3]"> Development</span>
         </h2>
-        <p class="text-[0.7rem] font-semibold sm:text-2xl ">
+        <p class="text-xs font-semibold sm:text-2xl">
           We have dedicated development Team
         </p>
-        <p class="py-3 text-lg">
+        <p class="py-3 text-xs text-justify sm:text-lg">
           Our expert team employs cutting-edge technologies and agile
           methodologies to deliver scalable, reliable, and customized software
           applications that align seamlessly with your business objectives. From
@@ -16,27 +16,27 @@
           solutions not only meet but exceed your expectations, empowering your
           organization to thrive in the digital landscape.
         </p>
-        <div class="flex gap-5 pt-6">
+        <div class="flex flex-col-reverse gap-3 py-3 r sm:pt-6 sm:gap-5 sm:flex-row">
           <button
-            class="flex capitalize items-center gap-3 py-1 px-6  outline outline-offset-1 outline-1  text-lg active:shadow-md transition-all font-bold text-[#0245A3] hover:text-[#0245a3b6]"
+            class="flex text-base py-3 justify-center capitalize items-center gap-3  px-3 sm:px-6  outline outline-offset-0 outline-1  sm:text-lg active:shadow-md transition-all font-bold text-[#0245A3] hover:text-[#0245a3b6]"
           >
             Free consultation
             <i
-              class="text-2xl transition-all duration-150 fa-solid fa-envelope"
+              class="transition-all duration-150 sm:text-2xl fa-solid fa-envelope"
             ></i>
           </button>
           <button
-            class="flex capitalize items-center bg-[#0245A3] hover:bg-[rgba(2,69,163,0.8)] gap-3 py-3 px-6 text-lg  active:shadow-md transition-all font-bold text-gray-50 "
+            class="flex capitalize border border-blue-800 text-base py-3  justify-center items-center bg-[#0245A3] outline-blue-800 outline-offset-1   outline-2 hover:bg-[rgba(2,69,163,0.8)] gap-3  px-6 sm:text-lg  active:shadow-md transition-all font-bold text-gray-50 "
           >
-            Check our rates
+             Our rates
             <i
-              class="text-2xl transition-all duration-150 fa-solid fa-dollar-sign"
+              class="transition-all duration-150 sm:text-2xl fa-solid fa-dollar-sign"
             ></i>
           </button>
         </div>
       </div>
       <div>
-        <img class="w-2/6 mx-auto sm:w-4/6" src="../assets/images/js-dev.png" alt="" />
+        <img class="w-3/6 mx-auto sm:w-4/6" src="../assets/images/js-dev.png" alt="" />
       </div>
     </div>
   </section>
